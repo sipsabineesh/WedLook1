3b053e5c68563e0844b152e8f5e0634cdbe47eed
Sipsa <sipsabineesh@gmail.com>
2023-11-06 18:21:52 +0530

    Gmail App Password Changed because of password change

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -180,7 +180,7 @@ const  sendEmail = async(email,message) => {
         service: 'gmail',
         auth: {
             user: 'sipsabineesh@gmail.com',
-            pass: 'gycisgobecmixzlc'
+            pass: 'bfqn ugjt gzmw kblg'
         }
     });
      
@@ -193,7 +193,7 @@ const  sendEmail = async(email,message) => {
      
    const info = await mailTransporter.sendMail(mailDetails, function(err, data) {
         if(err) {
-            console.log('Error Occurs');
+            console.log('Error Occurs'+err.message);
         } else {
             console.log('Email sent successfully');
         }
